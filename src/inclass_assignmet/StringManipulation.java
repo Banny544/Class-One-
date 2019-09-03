da4137a7f378ffcd12df40d3df9a6a9f496d4925
Banny544 <giovanniwells544@gmail.com>
2019-09-03 18:13:52 -0400

    Class One- String Manipulation

---
+++ src/inclass_assignmet/StringManipulation.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inclass_assignmet;
+
+/**
+ *
+ * @author Banny544
+ */
+public class StringManipulation {
+    public static void main(String[] args) {
+        String string1 = new String("Giovanni");
+        String string2 = new String("Giovanni");
+        System.out.println(string1 == string2);
+        System.out.println(string1.equals(string2));
+    }
+}
